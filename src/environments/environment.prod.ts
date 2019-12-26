acdf5fc77fc2bd32249286851213c7c57c18bb8c
Samuel P Williams <sampwil@gmail.com>
2019-12-25 22:26:17 -0500

    update api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://talent-metrics-api.herokuapp.com/',
+  apiUrl: 'https://talent-metrics-api.herokuapp.com/api',
   webUrl: 'https://talent-metrics-web.herokuapp.com/',
   apiKey: 'AIzaSyBOu9GFb1aMRub58iUoFoOsh1A7bIQH7pM'
 };
